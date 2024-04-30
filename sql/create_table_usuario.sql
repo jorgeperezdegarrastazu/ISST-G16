403c8f02269d897f37c7972adcdd83911a754c42
Jorge Pérez <jorge.perez.degarrastazu@alumnos.upm.es>
2024-04-30 13:12:31 +0200

    Inclusión de mvn y creación de tablas + organización de templates

---
+++ sql/create_table_usuario.sql
@@ -0,0 +1,11 @@
+CREATE TABLE usuario (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(100) NOT NULL,
+    edad INT NOT NULL,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    peso INT,
+    altura INT,
+    premium BOOLEAN
+);
