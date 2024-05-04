d7ee39e3a9f5ed9e61e6bbac5c4e443ade149ec1
Jorge Pérez <jorge.perez.degarrastazu@alumnos.upm.es>
2024-05-05 01:34:00 +0200

    autoincremento id en bbdd

--- src/main/java/com/nutriapp/model/User.java
+++ src/main/java/com/nutriapp/model/User.java
@@ -1,12 +1,14 @@
 package com.nutriapp.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class User {
     @Id 
-    //@GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombre;
     private int edad;
